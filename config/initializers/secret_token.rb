29cfb0df7a21e804de14d2e72f9bae0dd04ec494
El Ukro <el.ukro@gmail.com>
2014-04-16 10:31:25 +0300

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitapp::Application.config.secret_key_base = '955f4bf61c20c85735154ba2630b69512c741d15e5972d905d929c8831c6e173d5d70d7dde65fecf530f1cb0c75c87dbfecc1efb368b9939ab18f86147c05414'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitapp::Application.config.session_store :cookie_store, key: '_gitapp_session'
